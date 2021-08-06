c228b1d7144c29e185b31e6cf13acdd22af23d37
Thomas Petersen <petersen@temp.dk>
2021-08-06 13:27:25 +0200

    Testing a pipenv run

---
+++ rational-numbers/rational_numbers.py
@@ -0,0 +1,47 @@
+from __future__ import division
+
+
+class Rational:
+    def __init__(self, numer, denom):
+        cmn = Rational._gcd(numer, denom)
+        smaller_numer = numer // cmn
+        smaller_denom = denom // cmn
+        sign_correct = -1 if smaller_denom < 0 else 1
+        
+        self.numer = sign_correct * smaller_numer
+        self.denom = sign_correct * smaller_denom
+
+    @staticmethod
+    def _gcd(a, b):
+        if (b == 0):
+            return a
+        else:
+            return Rational._gcd(b, a%b)
+
+
+    def __eq__(self, other):
+        return self.numer == other.numer and self.denom == other.denom
+
+    def __repr__(self):
+        return '{}/{}'.format(self.numer, self.denom)
+
+    def __add__(self, other):
+        return Rational(self.numer*other.denom + self.denom*other.numer, self.denom*other.denom)
+
+    def __sub__(self, other):
+        return Rational(self.numer*other.denom - self.denom*other.numer, self.denom*other.denom)
+
+    def __mul__(self, other):
+        return Rational(self.numer*other.numer, self.denom*other.denom)
+
+    def __truediv__(self, other):
+        return Rational(self.numer*other.denom, self.denom*other.numer)
+
+    def __abs__(self):
+        return Rational(abs(self.numer), abs(self.denom))
+
+    def __pow__(self, power):
+        return Rational(self.numer**power, self.denom**power)
+
+    def __rpow__(self, base):
+        return base**(self.numer/self.denom)
