15be103a1bf8a46b2e8b66a9893b207b97d0bece
Thomas Petersen <petersen@temp.dk>
2021-08-06 14:58:48 +0200

    Only run on PR + linting

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -4,8 +4,8 @@ Wanted something that doesn't depend on casting the number to a string.
 def is_armstrong_number(number):
     """Test if a number is an armstrong number"""
     digits = list(generate_digits(number))
-    sz = len(digits)
-    acc = sum([d**sz for d in digits])
+    input_size = len(digits)
+    acc = sum([d**input_size for d in digits])
     return acc == number
 
 def generate_digits(number, base = 10):
